017aa3f9e567afa1251ac57bb02d64be4fc191b0
Dzmitry Piachynski <pechinskiy.dima@gmail.com>
2023-06-20 23:38:51 +0200

    Displaying photo collection from json and searching items by catchphrase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import './index.css';
+import {Collection} from './Collection'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [collections, setCollections] = useState([])
+
+    useEffect(() => {
+        fetch('https://64920b572f2c7ee6c2c95782.mockapi.io/collections')
+            .then(res => res.json())
+            .then(json => {
+                setCollections(json);
+            })
+            .catch(err => {
+                console.warn(err)
+                alert('Cant get collections')
+            })
+    }, [])
+
+    return (
+        <div className="App">
+            <h1>Моя коллекция фотографий</h1>
+            <div className="top">
+                <ul className="tags">
+                    <li className="active">Все</li>
+                    <li>Горы</li>
+                    <li>Море</li>
+                    <li>Архитектура</li>
+                    <li>Города</li>
+                </ul>
+                <input className="search-input" placeholder="Поиск по названию"/>
+            </div>
+            <div className="content">
+                {
+                    collections.map((obj, index) => (
+                        <Collection key={index} name={obj.name} images={obj.photos} />
+                    ))
+                }
+            </div>
+            <ul className="pagination">
+                <li>1</li>
+                <li className="active">2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
