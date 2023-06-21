c8be3268e124d3dddc392469ddc90d305c11cf81
Dzmitry Piachynski <pechinskiy.dima@gmail.com>
2023-06-21 11:12:41 +0200

    Filtration and pagination

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ import {Collection} from './Collection'
 function App() {
 
     const [collections, setCollections] = useState([])
+    const [searchValue, setSearchValue] = useState('')
+    const [categoryId, setCategoryId] =useState(0)
+    const [page, setPage] =useState(1)
+
+    const categories = [
+        {"name": "All"},
+        {"name": "Sea"},
+        {"name": "Mountains"},
+        {"name": "Architecture"},
+        {"name": "Cities"}
+    ]
 
     useEffect(() => {
-        fetch('https://64920b572f2c7ee6c2c95782.mockapi.io/collections')
+        const category = categoryId ? `category=${categoryId}` : ''
+        fetch(`https://64920b572f2c7ee6c2c95782.mockapi.io/collections?page=${page}&limit=6&${category}`)
             .then(res => res.json())
             .then(json => {
                 setCollections(json);
@@ -16,32 +28,48 @@ function App() {
                 console.warn(err)
                 alert('Cant get collections')
             })
-    }, [])
+    }, [categoryId, page])
 
     return (
         <div className="App">
-            <h1>Моя коллекция фотографий</h1>
+            <h1>My collection of photos</h1>
             <div className="top">
                 <ul className="tags">
-                    <li className="active">Все</li>
-                    <li>Горы</li>
-                    <li>Море</li>
-                    <li>Архитектура</li>
-                    <li>Города</li>
+                    {categories.map((obj, index) => (
+                            <li className={categoryId === index ? 'active' : ''}
+                                onClick={() => setCategoryId(index)}
+                                key={obj.name}>
+                                {obj.name}</li>
+                            )
+                        )
+                    }
                 </ul>
-                <input className="search-input" placeholder="Поиск по названию"/>
+                <input
+                    value={searchValue}
+                    onChange={(value) => setSearchValue(value.target.value)}
+                    className="search-input"
+                    placeholder="Search by title"/>
             </div>
             <div className="content">
                 {
-                    collections.map((obj, index) => (
+                    collections
+                        .filter(obj => obj.name.toLowerCase().includes(searchValue.toLowerCase()))
+                        .map((obj, index) => (
                         <Collection key={index} name={obj.name} images={obj.photos} />
                     ))
                 }
             </div>
             <ul className="pagination">
-                <li>1</li>
-                <li className="active">2</li>
-                <li>3</li>
+                {
+                    [...Array(5)].map((_, i) => (
+                        <li
+                            onClick={() => setPage(i + 1)}
+                            className={page === i + 1 ? 'active' : ''}
+                            key={i}
+                        >{i + 1}</li>
+                        )
+                    )
+                }
             </ul>
         </div>
     );
